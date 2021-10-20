cc5c445533334e671fcea571229f6a478e5b0a66
Darren Ho <darrenho97@hotmail.com>
2021-10-20 18:32:51 +0800

    changed dataset to face mask detection - amend csv converter

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -33,15 +33,16 @@ def xml_to_csv(path):
         root = tree.getroot()
         for member in root.findall("object"):
             classes_names.append(member[0].text)
+            print(member)
             value = (
                 root.find("filename").text,
                 int(root.find("size")[0].text),
                 int(root.find("size")[1].text),
                 member[0].text,
-                int(member[4][0].text),
-                int(member[4][1].text),
-                int(member[4][2].text),
-                int(member[4][3].text),
+                int(member[5][0].text),
+                int(member[5][1].text),
+                int(member[5][2].text),
+                int(member[5][3].text),
             )
             xml_list.append(value)
     column_name = [
